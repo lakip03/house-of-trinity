40699b72226d21059c28bd7621b3bf35b4f635ba
Nikola Acimovic <lakip03@gmail.com>
2025-08-07 10:25:05 +0200

    Morging push

---
+++ Assets/Scripts/Rules/RuleTimeBend.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TimeBend", menuName = "Rules/Movement/Time Bend")]
+public class TimeBend : Rule
+{
+
+    public override void ActivateRule(PlayerController player)
+    {
+        
+    }
+    public override void DeactivateRule(PlayerController player)
+    {
+        
+    }
+    public override void UpdateRule(PlayerController player, float deltaTime)
+    {
+        //TBA: Sound particles etc...
+    }
+}
